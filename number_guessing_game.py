09fc42c03effb21cdd318ca9f71c71ab80ad25d2
Bibin Babu <bibin6724@gmail.com>
2022-05-01 21:14:35 +0530

    some changes made in indents and started defining the pregame method

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,9 +9,9 @@ print("********************************************")
 print("********************************************")
 print("********************************************")
 answer=r.randint(0,24)
-try:
-    def easy():
-        print("please enter a number below 25")
+def easy():
+    print("please enter a number below 25")
+    try:
         user_guess=int(input(">"))
         if user_guess==answer:
             print("hurrey! your guess is correct")
@@ -22,34 +22,34 @@ try:
             print("sorry boss you went much more lower than the actual answer")
             easy()
 
-except ValueError:
-    print("please enter a number")
-    easy()
+    except ValueError:
+        print("please enter a number")
+        easy()
 
 
 def hard():
     print("please enter a number below 25")
-try:
-    user_guess=int(input(">"))
-    tries=0
-    while tries < 5:
-        if user_guess==answer:
-            print("boss you have guessed a correct answer :")
-            break
-        elif user_guess > answer:
-            print("sorry boss you went higher than the actual answer(*_*)")
-            tries +=1
-            if tries==5:
-                print(f"\nyou are failed you had {tries} tries")
-            else:
-                print("please guess the number: ")
-        elif user_guess < answer:
-            print("sorry boss you went lower than the actual answer(*_*)")
-            tries +=1
-            if tries == 5:
-                print(f"\nyou are failed you had {tries} tries")
-            else:
-                print("please guess the number: ")
+    try:
+        user_guess=int(input(">"))
+        tries=0
+        while tries < 5:
+            if user_guess==answer:
+                print("boss you have guessed a correct answer :")
+                break
+            elif user_guess > answer:
+                print("sorry boss you went higher than the actual answer(*_*)")
+                tries +=1
+                if tries==5:
+                    print(f"\nyou are failed you had {tries} tries")
+                else:
+                    print("please guess the number: ")
+            elif user_guess < answer:
+                print("sorry boss you went lower than the actual answer(*_*)")
+                tries +=1
+                if tries == 5:
+                    print(f"\nyou are failed you had {tries} tries")
+                else:
+                    print("please guess the number: ")
 
-except ValueError:
-    print("!!please enter a number")
+    except ValueError:
+        print("!!please enter a number")
